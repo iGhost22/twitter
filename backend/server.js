1bb1d2ab5ec4878e29ee0a5efb6384367cdd69bb
Rhymings <huynh.lam.dat.77@gmail.com>
2024-06-23 01:52:05 +0700

    push SignUp, Login, Logout

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI);
+
+app.use(express.json()); // to parse req.body
+
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-  connectMongoDB();
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
 });
