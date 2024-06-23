af838541a2d3c1672e273bd8b23053c8a99b2c06
Rhymings <huynh.lam.dat.77@gmail.com>
2024-06-23 15:31:10 +0700

    update post controller, model, route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -30,6 +32,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
 
 
 app.listen(PORT, () => {
