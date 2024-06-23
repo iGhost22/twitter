b3cac353fad7840e9c45be2ac8abb62c8fa8bb30
Rhymings <huynh.lam.dat.77@gmail.com>
2024-06-23 22:36:18 +0700

    create post function, limit updated server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -19,14 +19,10 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
-
-app.use(express.json()); // to parse req.body
-
+app.use(express.json({ limit: "5mb" })); 
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
@@ -35,16 +31,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
-
-
 
 
 app.listen(PORT, () => {
