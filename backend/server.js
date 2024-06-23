51de105a0cd192eece22b82370b7b5b4fe4811b9
Rhymings <huynh.lam.dat.77@gmail.com>
2024-06-23 15:53:55 +0700

    update notification

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
-
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -33,6 +33,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
 
 
 
